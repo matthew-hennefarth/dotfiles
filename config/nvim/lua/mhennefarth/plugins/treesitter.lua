583b6d3754cec38df87db8048a4969725d1d5e7a
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-03-11 19:57:16 -0600

    add fortran as a language

--- config/nvim/lua/mhennefarth/plugins/treesitter.lua
+++ config/nvim/lua/mhennefarth/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {"cpp", "python", "lua", "rust"},
+  ensure_installed = {"cpp", "python", "lua", "rust", "fortran"},
   sync_install = false,
   ignore_install = {""},
   highlight = {
