8f23036fde6ade80f3fe86e9e757c878ff1a54c2
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-05-23 15:19:32 -0500

    add upgrade pyscf script

---
+++ scripts/upgrade_pyscf.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh -l
+
+PYSCF_DIR=$HOME/Developer/pyscf
+PYSCF_FORGE_DIR=$HOME/Developer/pyscf-forge
+
+function upgrade() {
+  pushd $1
+
+  git fetch upstream
+  current_branch=`git branch | grep \* | cut -d' ' -f2`
+  git checkout master
+  if ! git merge upstream/master | grep "Already up to date."; then
+    git push
+
+    # Build new library
+    pushd pyscf/lib/build
+    cmake ..
+    make
+    popd
+  fi
+  git checkout $current_branch
+  popd
+}
+
+venv activate pyscf
+
+upgrade $PYSCF_DIR
+upgrade $PYSCF_FORGE_DIR
+
+venv deactivate
