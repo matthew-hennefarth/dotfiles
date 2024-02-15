a65bc7dc28473a3efab2789dac34ea2109f2b54c
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2024-02-15 10:38:34 -0600

    update tmux-power

--- scripts/tmux-battery.sh
+++ scripts/tmux-battery.sh
@@ -2,7 +2,7 @@
 
 is_charging(){
   status=`pmset -g batt | awk -F '; *' 'NR==2 { print $2 }'`
-  if [ "$status" != "discharging" ]; then
+  if [[ "$status" != "discharging" ]]; then
     return
   fi
   false
@@ -11,49 +11,49 @@ is_charging(){
 charging_symbol(){
   remaining=$1
   if is_charging; then
-    if [ "$remaining" -gt "95" ]; then 
+    if [[ "$remaining" -gt "95" ]]; then 
       echo "󰂅"
-    elif [ "$remaining" -gt "85" ]; then 
+    elif [[ "$remaining" -gt "85" ]]; then 
       echo "󰂋"
-    elif [ "$remaining" -gt "75" ]; then
+    elif [[ "$remaining" -gt "75" ]]; then
       echo "󰂊"
-    elif [ "$remaining" -gt "65" ]; then
+    elif [[ "$remaining" -gt "65" ]]; then
       echo "󰢞"
-    elif [ "$remaining" -gt "55" ]; then 
+    elif [[ "$remaining" -gt "55" ]]; then 
       echo "󰂉"
-    elif [ "$remaining" -gt "45" ]; then
+    elif [[ "$remaining" -gt "45" ]]; then
       echo "󰢝"
-    elif [ "$remaining" -gt "35" ]; then
+    elif [[ "$remaining" -gt "35" ]]; then
       echo "󰂈"
-    elif [ "$remaining" -gt "25" ]; then
+    elif [[ "$remaining" -gt "25" ]]; then
       echo "󰂇"
-    elif [ "$remaining" -gt "15" ]; then
+    elif [[ "$remaining" -gt "15" ]]; then
       echo "󰂆"
-    elif [ "$remaining" -gt "5" ]; then
+    elif [[ "$remaining" -gt "5" ]]; then
       echo "󰢜"
     else
       echo "󰢟"
     fi
   else
-    if [ "$remaining" -gt "95" ]; then 
+    if [[ "$remaining" -gt "95" ]]; then 
       echo "󰁹"
-    elif [ "$remaining" -gt "85" ]; then 
+    elif [[ "$remaining" -gt "85" ]]; then 
       echo "󰂂"
-    elif [ "$remaining" -gt "75" ]; then
+    elif [[ "$remaining" -gt "75" ]]; then
       echo "󰂁"
-    elif [ "$remaining" -gt "65" ]; then
+    elif [[ "$remaining" -gt "65" ]]; then
       echo "󰂀"
-    elif [ "$remaining" -gt "55" ]; then 
+    elif [[ "$remaining" -gt "55" ]]; then 
       echo "󰁿"
-    elif [ "$remaining" -gt "45" ]; then
+    elif [[ "$remaining" -gt "45" ]]; then
       echo "󰁾"
-    elif [ "$remaining" -gt "35" ]; then
+    elif [[ "$remaining" -gt "35" ]]; then
       echo "󰁽"
-    elif [ "$remaining" -gt "25" ]; then
+    elif [[ "$remaining" -gt "25" ]]; then
       echo "󰁼"
-    elif [ "$remaining" -gt "15" ]; then
+    elif [[ "$remaining" -gt "15" ]]; then
       echo "󰁻"
-    elif [ "$remaining" -gt "5" ]; then
+    elif [[ "$remaining" -gt "5" ]]; then
       echo "󰁺"
     else
       echo "󰂎"
@@ -62,8 +62,13 @@ charging_symbol(){
 }
 
 main(){
-  remaining=`pmset -g batt | grep -o "[0-9]\{1,3\}%" | cut -d% -f1`
-  symbol=$(charging_symbol $remaining)
-  echo $symbol $remaining% 
+  lines=`pmset -g batt | wc -l`
+  if [[ "$lines" -eq "1" ]]; then
+    echo "󰚥 AC"
+  else
+    remaining=`pmset -g batt | grep -o "[0-9]\{1,3\}%" | cut -d% -f1`
+    symbol=$(charging_symbol $remaining)
+    echo $symbol $remaining%
+  fi
 }
 main
