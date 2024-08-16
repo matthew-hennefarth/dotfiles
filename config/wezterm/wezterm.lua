56c9bce16424f059386a7e8a9ff150bb0e5e7338
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2024-08-16 09:21:19 -0500

    change to wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font_with_fallback({
+	"DinaRemasterII",
+	"GohuFont uni11 Nerd Font Propo",
+})
+config.font_size = 18
+config.bold_brightens_ansi_colors = false
+
+config.color_scheme = "GruvboxDarkHard"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+-- good for tiling window managers
+config.adjust_window_size_when_changing_font_size = false
+
+-- removes the title and decorations
+config.window_decorations = "RESIZE"
+
+return config
