161e03c62cc882cf993fd118c0e19b84d33040ab
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2025-06-02 15:32:19 -0500

    update slightly

--- scripts/boxsync.sh
+++ scripts/boxsync.sh
@@ -3,4 +3,7 @@
 REMOTE=uchicago-box
 LOCAL=$HOME/Documents/box
 
-rclone bisync --resync-mode newer --recover --max-lock 2m --conflict-resolve newer --resilient $REMOTE:/ $LOCAL/ --exclude-from ~/.config/rclone/exclude-file.txt --create-empty-src-dirs
+# Should be used if we need to resync...
+#rclone bisync --resync-mode newer --recover --max-lock 2m --conflict-resolve newer --resilient $REMOTE:/ $LOCAL/ --exclude-from ~/.config/rclone/exclude-file.txt --create-empty-src-dirs 
+
+rclone bisync --recover --max-lock 2m --conflict-resolve newer --resilient $REMOTE:/ $LOCAL/ --exclude-from ~/.config/rclone/exclude-file.txt --create-empty-src-dirs
